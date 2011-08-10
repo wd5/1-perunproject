e5b2c32fc7ca4b7eb01762f97c6ff46e3cbd1802
dfalk <tfalc@mail.ru>
2011-08-10 15:12:49 +0400

    events: fix published filter

--- fevents/managers.py
+++ fevents/managers.py
@@ -6,4 +6,4 @@ class PublicManager(Manager):
     """Returns published posts that are not in the future."""
 
     def published(self):
-        return self.get_query_set().filter(is_published=True, date__lte=datetime.datetime.now())
+        return self.get_query_set().filter(is_published=True)
