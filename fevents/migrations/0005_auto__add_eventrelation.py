b8732caff7c93aeaaaa0d573b3ed5677d81fe7a3
dfalk <tfalc@mail.ru>
2011-08-11 16:46:25 +0400

    events: contenttype migration

---
+++ fevents/migrations/0005_auto__add_eventrelation.py
@@ -0,0 +1,65 @@
+# encoding: utf-8
+import datetime
+from south.db import db
+from south.v2 import SchemaMigration
+from django.db import models
+
+class Migration(SchemaMigration):
+
+    def forwards(self, orm):
+        
+        # Adding model 'EventRelation'
+        db.create_table('fevents_eventrelation', (
+            ('id', self.gf('django.db.models.fields.AutoField')(primary_key=True)),
+            ('event', self.gf('django.db.models.fields.related.ForeignKey')(to=orm['fevents.Event'])),
+            ('content_type', self.gf('django.db.models.fields.related.ForeignKey')(to=orm['contenttypes.ContentType'])),
+            ('object_id', self.gf('django.db.models.fields.PositiveIntegerField')()),
+        ))
+        db.send_create_signal('fevents', ['EventRelation'])
+
+
+    def backwards(self, orm):
+        
+        # Deleting model 'EventRelation'
+        db.delete_table('fevents_eventrelation')
+
+
+    models = {
+        'contenttypes.contenttype': {
+            'Meta': {'ordering': "('name',)", 'unique_together': "(('app_label', 'model'),)", 'object_name': 'ContentType', 'db_table': "'django_content_type'"},
+            'app_label': ('django.db.models.fields.CharField', [], {'max_length': '100'}),
+            'id': ('django.db.models.fields.AutoField', [], {'primary_key': 'True'}),
+            'model': ('django.db.models.fields.CharField', [], {'max_length': '100'}),
+            'name': ('django.db.models.fields.CharField', [], {'max_length': '100'})
+        },
+        'fevents.etype': {
+            'Meta': {'ordering': "('order', 'title')", 'object_name': 'Etype'},
+            'id': ('django.db.models.fields.AutoField', [], {'primary_key': 'True'}),
+            'image': ('django.db.models.fields.files.ImageField', [], {'max_length': '100', 'null': 'True', 'blank': 'True'}),
+            'order': ('django.db.models.fields.PositiveIntegerField', [], {'null': 'True', 'blank': 'True'}),
+            'slug': ('django.db.models.fields.SlugField', [], {'max_length': '70', 'db_index': 'True'}),
+            'title': ('django.db.models.fields.CharField', [], {'max_length': '70'}),
+            'title_plural': ('django.db.models.fields.CharField', [], {'max_length': '70'})
+        },
+        'fevents.event': {
+            'Meta': {'ordering': "['-date']", 'object_name': 'Event'},
+            'date': ('django.db.models.fields.DateTimeField', [], {'default': 'datetime.datetime.now'}),
+            'date_mod': ('django.db.models.fields.DateTimeField', [], {'auto_now': 'True', 'blank': 'True'}),
+            'description': ('django.db.models.fields.TextField', [], {'null': 'True', 'blank': 'True'}),
+            'enable_comments': ('django.db.models.fields.BooleanField', [], {'default': 'False'}),
+            'id': ('django.db.models.fields.AutoField', [], {'primary_key': 'True'}),
+            'is_published': ('django.db.models.fields.BooleanField', [], {'default': 'True'}),
+            'slug': ('django.db.models.fields.SlugField', [], {'unique': 'True', 'max_length': '70', 'db_index': 'True'}),
+            'title': ('django.db.models.fields.CharField', [], {'max_length': '70'}),
+            'type': ('django.db.models.fields.related.ForeignKey', [], {'to': "orm['fevents.Etype']", 'null': 'True', 'blank': 'True'})
+        },
+        'fevents.eventrelation': {
+            'Meta': {'object_name': 'EventRelation'},
+            'content_type': ('django.db.models.fields.related.ForeignKey', [], {'to': "orm['contenttypes.ContentType']"}),
+            'event': ('django.db.models.fields.related.ForeignKey', [], {'to': "orm['fevents.Event']"}),
+            'id': ('django.db.models.fields.AutoField', [], {'primary_key': 'True'}),
+            'object_id': ('django.db.models.fields.PositiveIntegerField', [], {})
+        }
+    }
+
+    complete_apps = ['fevents']
