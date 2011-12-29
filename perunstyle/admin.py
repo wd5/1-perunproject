8c79a4cf1f44dc6332fafcf9204d04eeba14fb44
dfalk <tfalc@mail.ru>
2011-12-29 18:10:46 +0400

    fix unicode usernames in admin

---
+++ perunstyle/admin.py
@@ -0,0 +1,26 @@
+# -*- coding: utf-8 -*-
+
+from django import forms
+from django.utils.translation import ugettext, ugettext_lazy as _
+from django.contrib.auth.models import User
+from django.contrib import admin
+from django.contrib.auth.admin import UserAdmin
+from django.contrib.auth.forms import UserCreationForm, UserChangeForm
+import re
+
+class UnicodeUserCreationForm(UserCreationForm):
+    username = forms.RegexField(label=_("Username"), max_length=30, regex=re.compile(ur'^[\w.@+-]+$', re.UNICODE),
+        help_text = _("Required. 30 characters or fewer. Letters, digits and @/./+/-/_ only."),
+        error_messages = {'invalid': _("This value may contain only letters, numbers and @/./+/-/_ characters.")})
+
+class UnicodeUserChangeForm(UserChangeForm):
+    username = forms.RegexField(label=_("Username"), max_length=30, regex=re.compile(ur'^[\w.@+-]+$', re.UNICODE),
+        help_text = _("Required. 30 characters or fewer. Letters, digits and @/./+/-/_ only."),
+        error_messages = {'invalid': _("This value may contain only letters, numbers and @/./+/-/_ characters.")})
+
+class UnicodeUserAdmin(UserAdmin):
+    form = UnicodeUserChangeForm
+    add_form = UnicodeUserCreationForm
+
+admin.site.unregister(User)
+admin.site.register(User, UnicodeUserAdmin)
