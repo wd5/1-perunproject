4e0fd2833e9b18fea01121bfc6a23b6aa447ad48
dfalk <tfalc@mail.ru>
2012-01-02 02:12:18 +0400

    profiles autocreating

--- perunprofile/models.py
+++ perunprofile/models.py
@@ -2,6 +2,8 @@
 
 from django.db import models
 from django.contrib.auth.models import User
+from django.db.models import signals
+from perunprofile.signals import create_profile
 
 class UserProfile(models.Model):
     user = models.OneToOneField(User, primary_key=True)
@@ -14,3 +16,6 @@ class UserProfile(models.Model):
 
 User.profile = property(lambda u: UserProfile.objects.get_or_create(user=u)[0])
 User.get_profile = lambda u: UserProfile.objects.get_or_create(user=u)[0]
+
+# When model instance is saved, trigger creation of corresponding profile
+signals.post_save.connect(create_profile, sender=User)
