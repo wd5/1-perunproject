87e37b1683a3bb878d54d1da9c33ecbff7cbf77e
dfalk <tfalc@mail.ru>
2012-01-16 11:13:03 +0400

    add object pagination

--- perunutils/templatetags/paginator.py
+++ perunutils/templatetags/paginator.py
@@ -6,6 +6,7 @@
 #     http://www.tummy.com/Community/Articles/django-pagination/
 
 from django import template
+from django.conf import settings
 
 register = template.Library()
 
@@ -45,3 +46,28 @@ def paginator(context, adjacent_pages=2):
 
 register.inclusion_tag('paginator.html', takes_context=True)(paginator)
 
+
+DEFAULT_PAGINATION = getattr(settings, 'PAGINATION_DEFAULT_PAGINATION', 20)
+DEFAULT_WINDOW = getattr(settings, 'PAGINATION_DEFAULT_WINDOW', 20)
+
+@register.inclusion_tag('object_paginate.html', takes_context=True)
+def object_paginate(context, base_url, count, paginate_by=DEFAULT_PAGINATION, window=DEFAULT_WINDOW):
+    if not isinstance(paginate_by, int):
+        paginate_by = template.Variable(paginate_by)
+    if not isinstance(window, int):
+        window = template.Variable(paginate_by)
+    page_count = count / paginate_by
+    if count % paginate_by > 0:
+        page_count += 1
+    context['page_count'] = page_count
+    pages = []
+    if page_count == 1:
+        pass
+    elif window >= page_count:
+        pages = [e + 1 for e in range(page_count)]
+    else:
+        pages = [e + 1 for e in range(window-1)]
+    context['base_url'] = base_url
+    context['pages'] = pages
+    context['window'] = window
+    return context
